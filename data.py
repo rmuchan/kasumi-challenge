651d8dd2acbe9b74bacc91b2f21f98cbd8fa7d7d
Muchan <liuzh1773@buaa.edu.cn>
2020-07-09 09:13:47 +0800

    data access: use a tree-shaped cache

--- data.py
+++ data.py
@@ -5,19 +5,18 @@ from os import path
 class _Dir:
     def __init__(self, pth: str):
         self._path = pth
+        self._loaded = {}
 
     def __getattr__(self, item: str):
         item = item.replace('_', '-')
-        key = path.join(self._path, item)
-        if key not in _loaded:
-            if path.isdir(key):
-                _loaded[key] = _Dir(key)
+        if item not in self._loaded:
+            if path.isdir(path.join(self._path, item)):
+                self._loaded[item] = _Dir(path.join(self._path, item))
             else:
                 with open(path.join(self._path, f'{item}.json'), 'r') as f:
-                    _loaded[key] = json.load(f)
-        return _loaded[key]
+                    self._loaded[item] = json.load(f)
+        return self._loaded[item]
 
 
-_loaded = {}
 _root = _Dir(path.join(path.dirname(__file__), 'data'))
 __getattr__ = _root.__getattr__
