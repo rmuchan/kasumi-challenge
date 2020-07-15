ca34817363e07e32cd3b49f50bcf6707519a41ce
Muchan <liuzh1773@buaa.edu.cn>
2020-07-15 19:56:45 +0800

    rand: new randomize mode

--- rand.py
+++ rand.py
@@ -29,6 +29,12 @@ def biased(expect: float, min_: float) -> Tuple[float, str]:
     return min_ + level * (max_ - min_), appraise
 
 
+def cubic(min_: int, max_: int) -> int:
+    length = max_ - min_
+    y = (1 - random.random()) ** 3
+    return round(y * length + min_)
+
+
 def randomize(template: Any):
     if isinstance(template, list):
         return [randomize(x) for x in template]
@@ -40,6 +46,12 @@ def randomize(template: Any):
             return randomize(random.choice(template['values']))
         elif mode == 'uniform':
             return random.uniform(template['min'], template['max'])
+        elif mode == 'triangular':
+            return random.triangular(template['min'], template['max'])
+        elif mode == 'triangular_int':
+            return round(random.triangular(template['min'] - 0.49, template['max'] + 0.49))
+        elif mode == 'cubic':
+            return cubic(template['min'], template['max'])
         elif mode == 'biased':
             return biased(template['expect'], template['min'])
         else:
