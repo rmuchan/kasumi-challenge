a587f0c356179bace4e5b6541d29a89bf31fb246
Muchan <liuzh1773@buaa.edu.cn>
2020-07-17 20:05:36 +0800

    data: allow store

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import json
+import os
 from os import path
 
 
@@ -8,17 +9,26 @@ class _Dir:
         self._loaded = {}
 
     def __getattr__(self, item: str):
+        if not item or item[0] == '_':
+            raise AttributeError(item)
         item = item.replace('_', '-')
         if item not in self._loaded:
-            if path.isdir(path.join(self._path, item)):
-                self._loaded[item] = _Dir(path.join(self._path, item))
-            else:
+            if path.isfile(path.join(self._path, f'{item}.json')):
                 with open(path.join(self._path, f'{item}.json'), 'r') as f:
                     self._loaded[item] = json.load(f)
+            else:
+                self._loaded[item] = _Dir(path.join(self._path, item))
         return self._loaded[item]
 
     def __getitem__(self, item: str):
         return getattr(self, item)
+
+    def __setitem__(self, item: str, value):
+        item = item.replace('_', '-')
+        self._loaded[item] = value
+        os.makedirs(self._path, 0o755, exist_ok=True)
+        with open(path.join(self._path, f'{item}.json'), 'w') as f:
+            json.dump(value, f, ensure_ascii=False, indent=2)
 
 
 _root = _Dir(path.join(path.dirname(__file__), 'data'))
