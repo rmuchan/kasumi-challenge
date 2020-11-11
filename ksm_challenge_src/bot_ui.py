c9bc7c6687f8eb985426b5461435e15d7f9c3b5d
Muchan <liuzh1773@buaa.edu.cn>
2020-11-11 22:48:32 +0800

    防卡功能添加重试

--- ksm_challenge_src/bot_ui.py
+++ ksm_challenge_src/bot_ui.py
@@ -34,10 +34,14 @@ class BotContextUI(UI):
     async def do_send(self, msg: str) -> None:
         if self.at_sender and self._ctx['message_type'] != 'private':
             msg = '\n' + msg
-        try:
-            await self._bot.send(self._ctx, msg, at_sender=self.at_sender)
-        except aiocqhttp.ActionFailed:
-            self._bot.logger.error('Failed to send message', exc_info=True)
+        for retry in range(3):
+            if retry != 0:
+                await asyncio.sleep(3)
+            try:
+                await self._bot.send(self._ctx, msg, at_sender=self.at_sender)
+                return
+            except aiocqhttp.ActionFailed:
+                self._bot.logger.error('Failed to send message', exc_info=True)
 
     async def do_input(self) -> str:
         """
